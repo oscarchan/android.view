0a803ca39d10ca859ab8cd7e3f1b8f09206e5848
Oscar Chan <ochan@ochan-md.linkedin.biz>
2009-07-21 17:33:21 -0700

    add movement test case

--- src/android/view/MainActivity.java
+++ src/android/view/MainActivity.java
@@ -1,7 +1,11 @@
 package android.view;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,9 +13,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+    private DateFormat formatter = new SimpleDateFormat("kk:mm:ss");
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -87,6 +94,55 @@ public class MainActivity extends Activity
             }
         });
 
+     Button movementButton = (Button) this.findViewById(R.id.test_movement_map_overlay);
+     
+     movementButton.setOnClickListener(
+         new OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                Intent intent = new Intent(MainActivity.this, ViewMapActivity.class);
+                ArrayList<Location> list = new ArrayList<Location>(Arrays.asList(
+                    mockLocation("07:06:17", 37.429681, -121.907170, 1901), 
+                    mockLocation("07:06:59", 37.424589, -121.915846, 1965),
+                    mockLocation("07:07:15", 37.425376, -121.914608, 1521), 
+                    mockLocation("07:07:28", 37.426636, -121.917310, 1521), 
+                    mockLocation("07:07:33", 37.426479, -121.915996, 1521),
+                    mockLocation("07:08:59", 37.421055, -121.922358, 1768), 
+                    mockLocation("07:09:26", 37.420251, -121.939335, 2187), 
+                    mockLocation("07:10:50", 37.418237, -121.951347, 1184),
+                    mockLocation("07:10:55", 37.417588, -121.956692, 1184),
+                    mockLocation("07:11:25", 37.418874, -121.957873, 2069),
+                    mockLocation("07:11:32", 37.416328, -121.971568, 2069),
+                    mockLocation("07:12:08", 37.413821, -121.983176, 2312),
+                    mockLocation("07:12:16", 37.413802, -121.984220, 2312),
+                    mockLocation("07:12:28", 37.412750, -121.987662, 2266),
+                    mockLocation("07:13:23", 37.407458, -122.005904, 1289),
+                    mockLocation("07:13:40", 37.405388, -122.003737, 1289),
+                    mockLocation("07:13:57", 37.401032, -122.016000, 1888),
+                    mockLocation("07:14:21", 37.400462, -122.019362, 1888),
+                    mockLocation("07:14:56", 37.402135, -122.038585, 1894),
+                    mockLocation("07:15:08", 37.400441, -122.034507, 1888),
+                    mockLocation("07:15:50", 37.401006, -122.035866, 1888),
+                    mockLocation("07:16:16", 37.403605, -122.044684, 1815),
+                    mockLocation("07:16:19", 37.403115, -122.042651, 1815),
+                    mockLocation("07:16:29", 37.404095, -122.046716, 1815),
+                    mockLocation("07:17:12", 37.404427, -122.057646, 2184),
+                    mockLocation("07:17:23", 37.403418, -122.062668, 1460),
+                    mockLocation("07:17:31", 37.405071, -122.065474, 1460),
+                    mockLocation("07:18:18", 37.410900, -122.076672, 2059),
+                    mockLocation("07:19:48", 37.412988, -122.068843, 2934),
+                    mockLocation("07:20:53", 37.411944, -122.072758, 2059),
+                    mockLocation("07:20:56", 37.411596, -122.074062, 2059),
+                    mockLocation("07:25:06", 37.420618, -122.075779, 2194),
+                    mockLocation("07:37:53", 37.416803, -122.072311, 2194)
+                    ));
+
+                
+                intent.putParcelableArrayListExtra(ViewActivityConstants.INTENT_LOCATIONS, list);
+                startActivity(intent);
+            }
+        });
     
   	Button proximityButton = (Button) this.findViewById(R.id.sample_proximity);
   	
@@ -103,6 +159,27 @@ public class MainActivity extends Activity
 //  	startService(service)
 	}
 	
+	private long getTime(String dateString)
+	{
+	    try {
+            Date time = formatter.parse(dateString);
+            return time.getTime();
+        } catch (ParseException e) {
+            Toast.makeText(this, "unable to parse text: " +  e, Toast.LENGTH_SHORT);
+            return System.currentTimeMillis();
+        }
+	}
+
+    private Location mockLocation(String date, double lat, double lng, float accuracy)
+    {
+        Location location = new Location("network");
+        location.setTime(getTime(date));
+        location.setAccuracy(accuracy);
+        location.setLatitude(lat);
+        location.setLongitude(lng);
+        
+        return location;
+    }
 	
 	
 	
